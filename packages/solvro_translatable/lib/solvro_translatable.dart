7edc0210a9f15a60eba98fd090fd9ff52ab8a737
Szymon Kowaliński <szymon@kowalinski.dev>
2025-04-14 13:20:05 +0200

    chore: add packages templates

---
+++ packages/solvro_translatable/lib/solvro_translatable.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
+
+export 'src/solvro_translatable_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
