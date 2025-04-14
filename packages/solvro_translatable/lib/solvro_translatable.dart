eae0acb00c3471c7656130a9b55cc68e546cdae3
Szymon Kowaliński <szymon@kowalinski.dev>
2025-04-14 14:31:54 +0200

    feat(core): add translatable interface

--- packages/solvro_translatable/lib/solvro_translatable.dart
+++ packages/solvro_translatable/lib/solvro_translatable.dart
@@ -1,8 +1,2 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
-library;
-
-export 'src/solvro_translatable_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
+export "src/annotations.dart";
+export "src/translatable.dart" show TranslatableInterface;
