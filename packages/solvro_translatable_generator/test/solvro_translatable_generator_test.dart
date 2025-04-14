8064c7469a3006bfa337974ee28d39c9d1bfa50e
Szymon Kowaliński <szymon@kowalinski.dev>
2025-04-14 13:20:05 +0200

    chore: add packages templates

---
+++ packages/solvro_translatable_generator/test/solvro_translatable_generator_test.dart
@@ -0,0 +1,16 @@
+import 'package:solvro_translatable_generator/solvro_translatable_generator.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
